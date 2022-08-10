20dfff2a53b51d5cd829a99c7a1b889c5785095b
unknown <nicolasgonzalez470@gmail.com>
2022-08-10 02:26:32 -0300

    responsive and fix details

--- src/components/DetailCountry/DetailCountry.js
+++ src/components/DetailCountry/DetailCountry.js
@@ -33,40 +33,46 @@ export function DetailCountry(){
 
     return (
         <div className='DetailCountry'>
-            {!Object.keys(country) || (country.common.toLowerCase() !== name.toLowerCase())?
+            {
+                !Object.keys(country).length ? 
+                <><div>Loading...</div></>
+                :<>
+                {(country.common.toLowerCase() !== name.toLowerCase())?
             <><div>Loading...</div></> : 
             <>
-                <button className='back'>Back</button>
+                        <button className='back'>Back</button>
 
-                <div className='box-info-country'>
-                    <img src={country.flag} alt={`${country.common} flag`}/>
-                    <div className='right-box-info-country'>
-                        <h1>{country.common}</h1>
+                        <div className='box-info-country'>
+                            <img src={country.flag} alt={`${country.common} flag`}/>
+                            <div className='right-box-info-country'>
+                                <h1>{country.common}</h1>
 
-                        <article>
-                            <div className='left-top-info-country'>
-                                <p>Native name: <span>{country.native_name[1]}</span></p>
-                                <p>Population: <span>{country.population}</span></p>
-                                <p>Region: <span>{country.region}</span></p>
-                                <p>Subregion: <span>{country.subregion}</span></p>
-                                <p>Capital: <span>{country.capital}</span></p>
-                            </div>
-                            
-                            <div className='right-bottom-info-country'>
-                                <p>Top Level Domain: <span>{country.domain}</span></p>
-                                <p>Currencies: <span>{keys_currencies(country.currencies)}</span></p>
-                                <p>Languages: <span>{keys_language(country.languages)}</span></p>
-                            </div>
-                        </article>
+                                <article>
+                                    <div className='left-top-info-country'>
+                                        <p>Native name: <span>{country.native_name[1]}</span></p>
+                                        <p>Population: <span>{country.population}</span></p>
+                                        <p>Region: <span>{country.region}</span></p>
+                                        <p>Subregion: <span>{country.subregion}</span></p>
+                                        <p>Capital: <span>{country.capital}</span></p>
+                                    </div>
+                                    
+                                    <div className='right-bottom-info-country'>
+                                        <p>Top Level Domain: <span>{country.domain}</span></p>
+                                        <p>Currencies: <span>{keys_currencies(country.currencies)}</span></p>
+                                        <p>Languages: <span>{keys_language(country.languages)}</span></p>
+                                    </div>
+                                </article>
 
-                        {country.borders? 
-                        <BordersList borders={country.borders.join(',')} /> :
-                        null
-                        }
-                    </div> 
-                </div>
-            </>    
-        }
+                                {country.borders? 
+                                <BordersList borders={country.borders.join(',')} /> :
+                                null
+                                }
+                            </div> 
+                        </div>
+                    </>    
+                }
+                </>
+            }
 
         </div>
     )
